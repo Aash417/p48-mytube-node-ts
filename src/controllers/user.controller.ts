df5b6842e0deadeacb7c87fb391a0b86f3b26ff8
aashish_k <aashishkathat00@gmail.com>
2024-01-27 14:52:33 +0530

    wrote aggregate pipeline for getting userChannel

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -298,3 +298,68 @@ export const updateUserCoverImage = asyncHandler(
       .json(new ApiResponse(200, user, 'cover image updated successfully'));
   }
 );
+
+export const getUserChanelProfile = asyncHandler(
+  async (req: customRequest, res: Response) => {
+    const { userName } = req.params;
+
+    if (!userName.trim()) throw new ApiError(400, 'username is missing');
+
+    const channel: {}[] = await User.aggregate([
+      {
+        $match: { userName: userName?.toLowerCase() },
+      },
+      {
+        $lookup: {
+          from: 'subscriptions',
+          localField: '_id',
+          foreignField: 'channel',
+          as: 'subscribers',
+        },
+      },
+      {
+        $lookup: {
+          from: 'subscriptions',
+          localField: '_id',
+          foreignField: 'subscriber',
+          as: 'subscribedTo',
+        },
+      },
+      {
+        $addFields: {
+          subscriberCount: {
+            $size: 'subscriber',
+          },
+          channelSubscribedToCount: {
+            $size: 'subscribedTo',
+          },
+          isSubscribed: {
+            $cond: {
+              if: { $in: [req.user._id, 'subscribers.subscriber'] },
+              then: true,
+              else: false,
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          fullName: 1,
+          userName: 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1,
+          subscriberCount: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 1,
+        },
+      },
+    ]);
+
+    if (!channel.length) throw new ApiError(404, 'channel does not exits');
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, channel[0], 'channel fetched successfully'));
+  }
+);
