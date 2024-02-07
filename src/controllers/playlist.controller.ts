429870f86bf6b501ec84811508f801b11fb51433
aashish_k <aashishkathat00@gmail.com>
2024-02-08 00:30:47 +0530

    todo 15: get use playlist controller

--- src/controllers/playlist.controller.ts
+++ src/controllers/playlist.controller.ts
@@ -1,17 +1,20 @@
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
 import Playlist, { playlistType } from '../models/playlist.model';
 import { ApiError } from '../utils/ApiError';
 import { ApiResponse } from '../utils/ApiResponse';
 import { asyncHandler } from '../utils/asyncHandler';
 import { customRequest } from '../utils/helper';
+import { Types } from 'mongoose';
 
 export const createPlaylist = asyncHandler(
   async (req: customRequest, res: Response) => {
+    //TODO: create playlist
     try {
+      // 1. get name & description
       const { name, description = '' }: playlistType = req.body;
       if (!name) throw new ApiError(400, 'A playlist name is required.');
-      //TODO: create playlist
 
+      // 2. create playlist
       const playlist: playlistType = await Playlist.create({
         name,
         description,
@@ -19,6 +22,7 @@ export const createPlaylist = asyncHandler(
       });
       if (!playlist) throw new ApiError(500, 'Failed to create playlist.');
 
+      // 3. return response
       res.status(200).json(new ApiResponse(200, playlist, 'Playlist created.'));
     } catch (error) {
       res
@@ -30,8 +34,29 @@ export const createPlaylist = asyncHandler(
 
 export const getUserPlaylists = asyncHandler(
   async (req: Request, res: Response) => {
-    const { userId } = req.params;
     //TODO: get user playlists
+
+    try {
+      // 1. get user id
+      const { userId } = req.params;
+      if (!userId || !Types.ObjectId.isValid(userId))
+        throw new ApiError(400, 'Provide a valid user ID');
+
+      // 2. get playlists
+      const playlists: playlistType[] = await Playlist.aggregate([
+        {
+          $match: { owner: Types.ObjectId.createFromHexString(userId) },
+        },
+      ]);
+      if (!playlists) throw new ApiError(500, 'Failed to fetch playlist.');
+
+      // 3. return response
+      res.status(200).json(new ApiResponse(200, playlists, 'created.'));
+    } catch (error) {
+      res
+        .status(error.statusCode)
+        .json(new ApiResponse(error.statusCode, {}, error.message));
+    }
   }
 );
 
