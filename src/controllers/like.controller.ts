54051cf45f1d533bec01d563c22827ef8c22a1b0
aashish_k <aashishkathat00@gmail.com>
2024-02-09 23:28:32 +0530

    todo 20: added toggle for video like

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -1,14 +1,57 @@
 import { Request, Response } from 'express';
-import mongoose, { isValidObjectId } from 'mongoose';
-import { Like } from '../models/like.model';
+import { Types } from 'mongoose';
+import { Like, likeType } from '../models/like.model';
+import Video, { videoType } from '../models/video.model';
 import { ApiError } from '../utils/ApiError';
 import { ApiResponse } from '../utils/ApiResponse';
 import { asyncHandler } from '../utils/asyncHandler';
+import { customRequest } from './../utils/helper';
 
 export const toggleVideoLike = asyncHandler(
-  async (req: Request, res: Response) => {
-    const { videoId } = req.params;
+  async (req: customRequest, res: Response) => {
     //TODO: toggle like on video
+    try {
+      // 1. get video id
+      const { videoId } = req.params;
+      if (!videoId || !Types.ObjectId.isValid(videoId))
+        throw new ApiError(500, 'Provide a valid video ID');
+
+      // 2. check if video exists
+      const video: videoType = await Video.findById(videoId);
+      if (!video) throw new ApiError(400, 'No such video exists.');
+
+      let like: likeType, unlike;
+
+      if (await Like.findOne({ likedBy: req.user._id, video: videoId })) {
+        // 3. remove like
+        unlike = await Like.deleteOne({
+          likedBy: req.user._id,
+          video: videoId,
+        });
+      } else {
+        // 4. add like
+        like = await Like.create({
+          likedBy: req.user._id,
+          video: videoId,
+        });
+        if (!like) throw new ApiError(500, 'Failed to like');
+      }
+
+      // 5. return response
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            200,
+            unlike ? unlike : like,
+            'Toggled like successfully'
+          )
+        );
+    } catch (error) {
+      res
+        .status(error.statusCode)
+        .json(new ApiResponse(error.statusCode, {}, error.message));
+    }
   }
 );
 
--- src/models/like.model.ts
+++ src/models/like.model.ts
@@ -1,18 +1,50 @@
 import { Schema, Types, model } from 'mongoose';
 
-type likeType = {
-  comment: Types.ObjectId;
-  video: Types.ObjectId;
-  likeBy: Types.ObjectId;
-  tweet: Types.ObjectId;
-};
+export interface likeType {
+  comment?: {
+    prototype?: Types.ObjectId;
+    cacheHexString?: unknown;
+    generate?: {};
+    createFromTime?: {};
+    createFromHexString?: {};
+    createFromBase64?: {};
+    isValid?: {};
+  };
+  video?: {
+    prototype?: Types.ObjectId;
+    cacheHexString?: unknown;
+    generate?: {};
+    createFromTime?: {};
+    createFromHexString?: {};
+    createFromBase64?: {};
+    isValid?: {};
+  };
+  likedBy: {
+    prototype?: Types.ObjectId;
+    cacheHexString?: unknown;
+    generate?: {};
+    createFromTime?: {};
+    createFromHexString?: {};
+    createFromBase64?: {};
+    isValid?: {};
+  };
+  tweet?: {
+    prototype?: Types.ObjectId;
+    cacheHexString?: unknown;
+    generate?: {};
+    createFromTime?: {};
+    createFromHexString?: {};
+    createFromBase64?: {};
+    isValid?: {};
+  };
+}
 
 const likeSchema = new Schema<likeType>(
   {
-    comment: [{ type: Types.ObjectId, ref: 'Comment' }],
-    video: [{ type: Types.ObjectId, ref: 'Video' }],
-    likeBy: [{ type: Types.ObjectId, ref: 'User' }],
-    tweet: [{ type: Types.ObjectId, ref: 'Tweet' }],
+    comment: { type: Types.ObjectId, ref: 'Comment' },
+    video: { type: Types.ObjectId, ref: 'Video' },
+    likedBy: { type: Types.ObjectId, ref: 'User' },
+    tweet: { type: Types.ObjectId, ref: 'Tweet' },
   },
   { timestamps: true }
 );
