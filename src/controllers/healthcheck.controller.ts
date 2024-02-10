ce9f39339e581932ddbbca6e5229e2641e613b95
aashish_k <aashishkathat00@gmail.com>
2024-02-10 23:46:58 +0530

    todo 29: final controller healthcheck

--- src/controllers/healthcheck.controller.ts
+++ src/controllers/healthcheck.controller.ts
@@ -1,8 +1,18 @@
 import { Request, Response } from 'express';
-import { ApiError } from '../utils/ApiError';
 import { ApiResponse } from '../utils/ApiResponse';
 import { asyncHandler } from '../utils/asyncHandler';
 
-export const healthcheck = asyncHandler(async (req: Request, res: Response) => {
-  //TODO: build a healthcheck response that simply returns the OK status as json with a message
-});
+export const healthcheck = asyncHandler(
+  async (_req: Request, res: Response) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { status: 'ok' },
+          'everything is working correctly.'
+        )
+      );
+  }
+);
