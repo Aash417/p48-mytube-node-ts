585d6d012a0b52941e18fab1dc05957ad1e47e7e
aashish_k <aashishkathat00@gmail.com>
2024-02-10 23:26:45 +0530

    todo 27: dashboard stats controller

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,9 +1,82 @@
 import { Request, Response } from 'express';
+import { Types } from 'mongoose';
+import Video from '../models/video.model';
+import { ApiError } from '../utils/ApiError';
+import { ApiResponse } from '../utils/ApiResponse';
 import { asyncHandler } from '../utils/asyncHandler';
 
 export const getChannelStats = asyncHandler(
   async (req: Request, res: Response) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    try {
+      const { channelId } = req.params;
+      const stats = await Video.aggregate([
+        {
+          $match: {
+            owner: new Types.ObjectId(channelId),
+          },
+        },
+        {
+          $lookup: {
+            from: 'Likes',
+            localField: '_id',
+            foreignField: 'video',
+            as: 'Likes',
+          },
+        },
+        {
+          $addFields: {
+            likes: {
+              $size: { $ifNull: ['$likes', []] },
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: 'subscriber',
+            localField: 'owner',
+            foreignField: 'channel',
+            as: 'subscriber',
+          },
+        },
+        {
+          $addFields: {
+            subscriber: {
+              $size: { $ifNull: ['$subscriber', []] },
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            totalViews: {
+              $sum: '$views',
+            },
+            totalVideos: {
+              $sum: 1,
+            },
+
+            totalLikes: {
+              $sum: '$likes',
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            owner: 0,
+          },
+        },
+      ]);
+      if (!stats) new ApiError(500, 'Failed to fetch channel stats');
+
+      // 4. return response
+      return res.status(200).json(new ApiResponse(200, stats, 'done'));
+    } catch (error) {
+      res
+        .status(error.statusCode)
+        .json(new ApiResponse(error.statusCode, {}, error.message));
+    }
   }
 );
 
