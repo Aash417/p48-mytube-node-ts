9730a492e3dc9a8bc47a4f48e5290973f4ea2966
aashish_k <aashishkathat00@gmail.com>
2024-02-11 15:15:27 +0530

    refactored like controller

--- src/controllers/like.controller.ts
+++ src/controllers/like.controller.ts
@@ -1,7 +1,7 @@
 import { Response } from 'express';
-import { Types } from 'mongoose';
+import { Types, Model as mongooseModel } from 'mongoose';
 import Comment, { commentType } from '../models/comment.model';
-import Like, { likeType } from '../models/like.model';
+import Like from '../models/like.model';
 import Tweet, { tweetType } from '../models/tweet.model';
 import Video, { videoType } from '../models/video.model';
 import { ApiError } from '../utils/ApiError';
@@ -9,34 +9,54 @@ import { ApiResponse } from '../utils/ApiResponse';
 import { asyncHandler } from '../utils/asyncHandler';
 import { customRequest } from './../utils/helper';
 
-export const toggleVideoLike = asyncHandler(
-  async (req: customRequest, res: Response) => {
-    //TODO: toggle like on video
+export const toggleLike = (model: string) => {
+  return asyncHandler(async (req: customRequest, res: Response) => {
     try {
-      // 1. get video id
-      const { videoId } = req.params;
-      if (!videoId || !Types.ObjectId.isValid(videoId))
-        throw new ApiError(500, 'Provide a valid video ID');
+      // 1. check which model is called
+      let Model: mongooseModel<commentType | videoType | tweetType>;
+      switch (model) {
+        case 'video':
+          Model = Video;
+          break;
 
-      // 2. check if video exists
-      const video: videoType = await Video.findById(videoId);
-      if (!video) throw new ApiError(400, 'No such video exists.');
+        case 'tweet':
+          Model = Tweet;
+          break;
 
-      let like: likeType, unlike;
+        case 'comment':
+          Model = Comment;
+          break;
 
-      if (await Like.findOne({ likedBy: req.user._id, video: videoId })) {
-        // 3. remove like
-        unlike = await Like.deleteOne({
-          likedBy: req.user._id,
-          video: videoId,
-        });
-      } else {
-        // 4. add like
-        like = await Like.create({
-          likedBy: req.user._id,
-          video: videoId,
-        });
+        default:
+          break;
+      }
+      if (!Model) throw new ApiError(500, 'Failed to assign Model');
+
+      // 2. get resource id
+      let resourceId = Object.entries(req.params)[0][1];
+      if (!resourceId || !Types.ObjectId.isValid(resourceId))
+        throw new ApiError(500, `Provide a valid ${model} ID`);
+
+      // 3. check if resource exists
+      const resource = await Model.findById(resourceId);
+      if (!resource) throw new ApiError(400, `No such ${resource} exists.`);
+
+      // toggle like
+      let like, unlike;
+      const searchObj = {
+        likedBy: req.user._id,
+        [model]: resourceId,
+      };
+
+      const checkLikeStatus = await Like.find(searchObj);
+      if (checkLikeStatus.length === 0) {
+        // 3. add like
+        like = await Like.create(searchObj);
         if (!like) throw new ApiError(500, 'Failed to like');
+      } else {
+        // 4. remove like
+        unlike = await Like.deleteOne(searchObj);
+        if (!unlike) throw new ApiError(500, 'Failed to unlike');
       }
 
       // 5. return response
@@ -46,7 +66,7 @@ export const toggleVideoLike = asyncHandler(
           new ApiResponse(
             200,
             unlike ? unlike : like,
-            'Toggled like successfully'
+            unlike ? `${model} unliked` : `${model} liked`
           )
         );
     } catch (error) {
@@ -54,104 +74,8 @@ export const toggleVideoLike = asyncHandler(
         .status(error.statusCode)
         .json(new ApiResponse(error.statusCode, {}, error.message));
     }
-  }
-);
-
-export const toggleCommentLike = asyncHandler(
-  async (req: customRequest, res: Response) => {
-    //TODO: toggle like on comment
-    try {
-      // 1. get comment id
-      const { commentId } = req.params;
-      if (!commentId || !Types.ObjectId.isValid(commentId))
-        throw new ApiError(500, 'Provide a valid comment ID');
-
-      // 2. check if comment exists
-      const comment: commentType = await Comment.findById(commentId);
-      if (!comment) throw new ApiError(400, 'No such comment exists.');
-
-      let like: likeType, unlike;
-
-      if (await Like.findOne({ likedBy: req.user._id, comment: commentId })) {
-        // 3. remove like
-        unlike = await Like.deleteOne({
-          likedBy: req.user._id,
-          comment: commentId,
-        });
-      } else {
-        // 4. add like
-        like = await Like.create({
-          likedBy: req.user._id,
-          comment: commentId,
-        });
-        if (!like) throw new ApiError(500, 'Failed to like');
-      }
-
-      // 5. return response
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(
-            200,
-            unlike ? unlike : like,
-            'Toggled like successfully'
-          )
-        );
-    } catch (error) {
-      res
-        .status(error.statusCode)
-        .json(new ApiResponse(error.statusCode, {}, error.message));
-    }
-  }
-);
-
-export const toggleTweetLike = asyncHandler(
-  async (req: customRequest, res: Response) => {
-    //TODO: toggle like on tweet
-    try {
-      // 1. get tweet id
-      const { tweetId } = req.params;
-      if (!tweetId || !Types.ObjectId.isValid(tweetId))
-        throw new ApiError(500, 'Provide a valid tweet ID');
-
-      // 2. check if tweet exists
-      const tweet: tweetType = await Tweet.findById(tweetId);
-      if (!tweet) throw new ApiError(400, 'No such tweet exists.');
-
-      let like: likeType, unlike;
-
-      if (await Like.findOne({ likedBy: req.user._id, tweet: tweetId })) {
-        // 3. remove like
-        unlike = await Like.deleteOne({
-          likedBy: req.user._id,
-          tweet: tweetId,
-        });
-      } else {
-        // 4. add like
-        like = await Like.create({
-          likedBy: req.user._id,
-          tweet: tweetId,
-        });
-        if (!like) throw new ApiError(500, 'Failed to like');
-      }
-
-      // 5. return response
-      return res
-        .status(200)
-        .json(
-          new ApiResponse(
-            200,
-            unlike ? unlike : like,
-            'Toggled like successfully'
-          )
-        );
-    } catch (error) {
-      res
-        .status(error.statusCode)
-        .json(new ApiResponse(error.statusCode, {}, error.message));
-    }
-  }
-);
+  });
+};
 
 export const getLikedVideos = asyncHandler(
   async (req: customRequest, res: Response) => {
@@ -163,7 +87,7 @@ export const getLikedVideos = asyncHandler(
         video: { $exists: true },
       });
       if (!videos) throw new ApiError(500, 'Failed to fetch liked videos');
-      // 5. return response
+      // . return response
       return res
         .status(200)
         .json(new ApiResponse(200, videos, 'Like videos fetched successfully'));
--- src/routes/like.routes.ts
+++ src/routes/like.routes.ts
@@ -1,18 +1,17 @@
 import { Router } from 'express';
-import {
-  getLikedVideos,
-  toggleCommentLike,
-  toggleVideoLike,
-  toggleTweetLike,
-} from '../controllers/like.controller';
+import { getLikedVideos, toggleLike } from '../controllers/like.controller';
 import { verifyJWT } from '../middlewares/auth.middleware';
 
 const router: Router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route('/toggle/v/:videoId').post(toggleVideoLike);
-router.route('/toggle/c/:commentId').post(toggleCommentLike);
-router.route('/toggle/t/:tweetId').post(toggleTweetLike);
+// router.route('/toggle/v/:videoId').post(toggleVideoLike);
+// router.route('/toggle/c/:commentId').post(toggleCommentLike);
+// router.route('/toggle/t/:tweetId').post(toggleTweetLike);
+router.route('/toggle/t/:tweetId').post(toggleLike('tweet'));
+router.route('/toggle/v/:tweetId').post(toggleLike('video'));
+router.route('/toggle/c/:tweetId').post(toggleLike('comment'));
+
 router.route('/videos').get(getLikedVideos);
 
 export default router;
